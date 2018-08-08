77bea2bd581a2d83d7fa20f52413a573221625f5
VishnuPalissery <vishnupalissery@qburst.com>
2018-08-08 17:36:12 +0530

    evng submission

--- src/Components/OutputList.js
+++ src/Components/OutputList.js
@@ -20,9 +20,11 @@ const outputList = (props) => {
         color: 'red'
     }
     let search_data = props.textdata;
+    let c =0;
     const name1 = props.list.map((element,index) => {  //array of elements in stock
 
         if(element.inStock === true){
+            c++;
             return (<tr key = {index}>
                 <td style = {table}>{element.name}</td>
                 <td style = {table}>{element.price}</td>
@@ -31,6 +33,7 @@ const outputList = (props) => {
     });
     const name2 = props.list.map((element,index) => { //array contains all elements
         if(element.inStock === true){
+
             return (<tr key = {index}>
                 <td style = {table}>{element.name}</td>
                 <td style = {table}>{element.price}</td>
@@ -57,21 +60,35 @@ const outputList = (props) => {
     </div>);
 }
 else
- {                                                   //serached display
-    const searchedProduct= props.list.filter(({name})=>(name.includes(search_data)))
+ {   
+     let searchedProduct;
+     if(props.check)  {
+        searchedProduct= props.list.filter(({name,inStock})=>((name.toLowerCase()).includes(search_data.toLowerCase()) && inStock == true))
+     }                                              //search display
+   else{
+    searchedProduct= props.list.filter(({name})=>((name.toLowerCase()).includes(search_data.toLowerCase())))
+   }
     let name3;
 
     if(searchedProduct.length!==0){
-        console.log(searchedProduct);
+        //console.log(searchedProduct);
 
      name3=searchedProduct.map((element,index) => {
-         if(element.inStock === true){
+
+         if(element.inStock === props.true){
             return (<tbody><tr key = {index}>
                 <td style = {table}>{element.name}</td>
                 <td style = {table}>{element.price}</td>
-            
-            </tr></tbody>);}
+            </tr></tbody>);
+
+         }
             else{
+                if(element.inStock === true){
+                    return (<tbody><tr key = {index}>
+                        <td style = {table}>{element.name}</td>
+                        <td style = {table}>{element.price}</td>
+                    
+                    </tr></tbody>);}
                 return (<tbody><tr key = {index}>
                     <td style = {tr}>{element.name}</td>
                     <td style = {tr}>{element.price}</td>
@@ -93,22 +110,10 @@ else
             </table>
         </div>);
     }else{
-        window.alert("Not Found,Try Again..!")  
+       // window.alert("Not Found,Try Again..!")  
        
                                              //notfound message 
-    return(""
-        /*<div style = {style}>
-        <table style = {table}>
-        <thead>
-            <tr>
-                <th style = {table}>Name</th>
-                <th style = {table}>Price</th>
-            </tr>
-            </thead>
-
-            {props.check ? name1 : name2}
-        </table>
-    </div>*/);
+    return(<h1 style={tr}>No Items Found,Try Again...!</h1>);
     }    
  }
 };
